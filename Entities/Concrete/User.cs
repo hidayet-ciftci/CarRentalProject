e1bf67ebbe86ef478c7e3cfed076e583270ef337
Heyedos <hidosss@hotmail.com>
2026-04-27 17:22:27 +0300

    start to add auth

--- Entities/Concrete/User.cs
+++ Entities/Concrete/User.cs
@@ -14,8 +14,7 @@ namespace Core.Entities.Concrete
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public byte[] PasswordSalt { get; set; }
-        public byte[] PasswordHash { get; set; }
+        public string PasswordHash { get; set; }
         public string Phone { get; set; }
         public bool Status { get; set; }
         public DateTime CreatedTime { get; set; }
